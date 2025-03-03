f0ac6610bcc429f229aa54a45d6884c89ac19474
vsctchaney <vsctchaney@pop-os.tail43230.ts.net>
2025-03-03 16:48:38 -0500

    Basic drawing and movement with pygame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     print("Starting Asteroids!")
@@ -7,12 +9,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    Player1 = Player(x = SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        Player1.update(dt)
         pygame.Surface.fill(screen, (0, 0, 0))
+        Player1.draw(screen)
+        dt = clock.tick(60)/1000
         pygame.display.flip()
 
 if __name__ == "__main__":
